50aac1528e8e556fec6cd4bd10007b1584a71651
dksavage1 <54415301+dksavage1@users.noreply.github.com>
2019-11-12 14:58:50 -0500

    Add files via upload

---
+++ loanchart.py
@@ -0,0 +1,9 @@
+#Write python program that generate a table that display future value of $1 at the end of n periods.
+# The formula is FutureValue = ( 1 + i)^n. No credits will be given if you don't use nested loop.
+n=0
+for n in range(1,n+1):
+    
+    for i in [4.0]:
+        futureValue = ( 1 + i)**n
+        print(f"{payment:.4f}",end=" ")
+    print()
